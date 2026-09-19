a957777dcc631764e73e581c734de020b8a05b75
Sheikh Sabbir <144557992+sheikhsabbir17@users.noreply.github.com>
2026-09-19 20:45:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+const form = document.getElementById("form");
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
+    const checkbox = document.getElementById("checkbox");
+    const existing = document.getElementById("existing");
+
+    if (localStorage.getItem("username")) {
+      existing.style.display = "block";
+    }
+
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+
+      alert(`Logged in as ${username.value}`);
+
+      if (checkbox.checked) {
+        localStorage.setItem("username", username.value);
+        localStorage.setItem("password", password.value);
+        existing.style.display = "block";
+      } else {
+        localStorage.removeItem("username");
+        localStorage.removeItem("password");
+        existing.style.display = "none";
+      }
+    });
+
+    existing.addEventListener("click", function() {
+      const savedUsername = localStorage.getItem("username");
+      alert(`Logged in as ${savedUsername}`);
+    });
